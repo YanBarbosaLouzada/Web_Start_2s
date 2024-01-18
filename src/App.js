6d2a668c3e235832801f7c91f9543edd335a68ef
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2024-01-18 15:23:52 -0300

    Trabalhando com UseState&UseEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  // const[variavel, functionParaMudarValorDaVariavel]=useState("Valor inicial");
+  const [useEffectCounter, setUseEffectCounter] = useState(0);
+  const [logado, setLogado] = useState(false)
+
+  useEffect(() => {
+    console.log("useEffect chamado");
+    setUseEffectCounter(useEffectCounter + 1);
+  }, [logado]);
+
+  const Logar = () => {
+    setLogado(true);
+  }
+  const Deslogar = () => {
+    setLogado(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1> useEffect foi chamado {useEffectCounter} vezes </h1>
+      <h2>UseEffect(Contador) é chamado quando o estado da variavel LOGADO E DESLOGADO muda de um para o outro</h2>
+      <h2>useState(estado) é chamado para trocar o estado da variavel </h2>
+
+      {/* If e else ternario para html */}
+      {logado ? <p>Logado</p> : <p>Deslogado</p>}
+
+      <button onClick={Logar}>Logar</button>
+      <button onClick={Deslogar}>Deslogar</button>
+
     </div>
   );
 }
